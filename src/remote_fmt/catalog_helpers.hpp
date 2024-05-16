b388c2229162863d6781aa057cd1d19fd4d1fc96
dominic <mail@dominicp.de>
2024-05-16 13:38:13 +0200

    added error message customization point

--- src/remote_fmt/catalog_helpers.hpp
+++ src/remote_fmt/catalog_helpers.hpp
@@ -1,22 +1,22 @@
 #pragma once
 #include <cstdint>
 #include <fmt/format.h>
+#include <fstream>
 #include <map>
 #include <nlohmann/json.hpp>
 #include <string>
-#include <fstream>
 
 namespace remote_fmt {
 
-inline std::map<std::uint16_t, std::string> parseStringConstantsFromJsonFile(std::string const& file) {
+inline std::pair<std::map<std::uint16_t, std::string>, std::string>
+parseStringConstantsFromJsonFile(std::string const& file) {
     try {
         std::ifstream        f(file);
         nlohmann::json const data = nlohmann::json::parse(f);
 
-        return data["StringConstants"].get<std::map<std::uint16_t, std::string>>();
+        return {data["StringConstants"].get<std::map<std::uint16_t, std::string>>(), ""};
     } catch(std::exception const& e) {
-        fmt::print(stderr, "read format strings failed: {}", e.what());
-        return {};
+        return {{}, fmt::format("read format strings failed: {}", e.what())};
     }
 }
 }   // namespace remote_fmt
